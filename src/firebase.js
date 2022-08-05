07340da679e36fc8de48a3cab63df9a30654e47b
MUHAMMADFAISAL123456 <50357691+MUHAMMADFAISAL123456@users.noreply.github.com>
2022-08-05 11:20:49 +0400

    fully funtional application of contact form with firesbase integartion
    i just connected this project of react with my local account

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,16 @@
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-  // Your firebase credentials
+    // Your web app's Firebase configuration
+
+    apiKey: "AIzaSyCzraJiomkdZuMnCdweOcjFLmgQmfD-ZB0",
+    authDomain: "react-contact-form-d5e55.firebaseapp.com",
+    projectId: "react-contact-form-d5e55",
+    storageBucket: "react-contact-form-d5e55.appspot.com",
+    messagingSenderId: "1051870881215",
+    appId: "1:1051870881215:web:13a6e2b790671ad4b02d0a"
+    
+
 });
 
 var db = firebaseApp.firestore();
